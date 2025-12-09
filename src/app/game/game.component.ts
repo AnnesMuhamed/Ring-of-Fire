3988dfe97cee69134ced73c5e032baf721ce889a
Annes Muhamed <aernnesto89@gmail.com>
2025-12-09 12:58:37 +0100

    Cards deck Added and animated

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForOf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, NgStyle],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation: boolean = false;
 
+  takeCard() {
+    this.pickCardAnimation = true;
+    
+  }
+
+  constructor() { }
 }
