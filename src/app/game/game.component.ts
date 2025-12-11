27cbb40ceb4731c8d98e50e2ff30fb111a441479
Annes Muhamed <aernnesto89@gmail.com>
2025-12-11 13:29:58 +0100

    Added Angular Material and introduced an Add button using the Material design components.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { NgForOf, NgIf, NgStyle} from '@angular/common';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [NgForOf, NgIf, NgStyle, PlayerComponent],
+  imports: [NgForOf, NgIf, NgStyle, PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
