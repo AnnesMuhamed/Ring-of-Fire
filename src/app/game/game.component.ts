52747baf6c98c331ac08936c7812836a8bd504ca
Annes Muhamed <aernnesto89@gmail.com>
2025-12-19 17:30:12 +0100

    Next player is selected.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -38,7 +38,8 @@ export class GameComponent implements OnInit {
       console.log(this.currentCard);
       this.pickCardAnimation = true;
       
-    
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
